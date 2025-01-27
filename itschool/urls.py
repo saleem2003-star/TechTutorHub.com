f21e4b98911f7d85823db097ddf50d567d7f0190
vanipenta saleem <vanipentashaiksaleem@gmail.com>
2025-01-27 14:05:38 +0530

    Update urls.py
    open directly

--- itschool/urls.py
+++ itschool/urls.py
@@ -23,7 +23,7 @@ from django.views.generic import TemplateView
 from it.views import signuppage,signinpage,admin_profile
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/',TemplateView.as_view(template_name='homepage.html'),name='home'),
+    path('',TemplateView.as_view(template_name='homepage.html'),name='home'),
     path('navbar/',TemplateView.as_view(template_name='navbar.html')),
     path("signup/",signuppage),
     path("signin/",signinpage),
